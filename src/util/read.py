bf1bd914294435f62a80df734c0a53dd49f8d1e4
enpuyou <youe2@allegheny.edu>
2019-10-02 12:59:55 -0400

    move readfile to analyzer module

--- src/util/read.py
+++
@@ -1,8 +0,0 @@
-"""Read file"""
-
-
-def read_file(filepath):
-    """Read the value from the filepath"""
-    with open(filepath) as file:
-        chosen_line = file.readline().replace("\n", "")
-        return chosen_line
