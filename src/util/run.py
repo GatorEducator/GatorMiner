201f16a2c4aa861759ed0ef38bd6c0c04de926c8
enpuyou <youe2@allegheny.edu>
2019-10-02 14:11:25 -0400

    move main to run.py

--- src/util/run.py
+++ src/util/run.py
@@ -1,4 +1,8 @@
 """Entry point"""
+import sys
+from analyzer import read_file
+from analyzer import word_freq
 
-
-# if __name__ == '__main__':
+if __name__ == '__main__':
+    freq = word_freq(read_file(sys.argv[1]))
+    print(freq)
