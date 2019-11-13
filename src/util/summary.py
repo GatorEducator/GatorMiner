4092f27cbe12763df7f9c64603819087e55043e5
Isaac Barrezueta <barrezuetai@allegheny.edu>
2019-11-13 10:54:37 -0500

    updated getText() and changed argument in summarize Text

--- src/util/summary.py
+++ src/util/summary.py
@@ -5,12 +5,19 @@ import os
 
 def summarizeText(text: str) -> str:
     """ Uses genim's summarization to summarize the given text """
-    return summarize(text, keywords=20)
+    return summarize(text, word_count=20)
 
 
 def getText(fileName: str) -> str:
     """ Returns selected passages from the file after given a file name """
-    pass
+    with open(fileName, "r") as file:
+        text = file.read()
+
+    text = text.replace("## What was the greatest technical challenge that your team faced and how did you overcome it?",
+    "## Based on your experiences with simple DNA manipulation in this lab and reflecting on the assigned article, answer the following questions:")
+
+    a = text.split("## Based on your experiences with simple DNA manipulation in this lab and reflecting on the assigned article, answer the following questions:")
+    return a[1]
 
 
 def getFileNames() -> [str]:
@@ -31,4 +38,4 @@ if __name__ == '__main__':
     fileNames = getFileNames()  # The directory is currently hardcoded
     for file in fileNames:
         text = summarizeText(getText(file))
-        pprint(f"{file}:\t{text}", end="\n-----------------\n")
+        print(f"{file}:\t{text}", end="\n-----------------\n")
