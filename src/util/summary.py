14092c3b46ced44a6bc11eafcd5c210d24ecf3ad
Isaac Barrezueta <barrezuetai@allegheny.edu>
2019-11-13 10:27:04 -0500

    updated getfiles function

--- src/util/summary.py
+++ src/util/summary.py
@@ -1,5 +1,6 @@
 from gensim.summarization import summarize, keywords
 from pprint import pprint
+import os
 
 
 def summarizeText(text: str) -> str:
@@ -15,11 +16,18 @@ def getText(fileName: str) -> str:
 def getFileNames() -> [str]:
     """ Uses os library to find all markdown files in given directory """
 
-    dirName = "../../cs100f2019_lab05_reflections/"
+    directory = "cs100f2019_lab05_reflections"
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".md"):
+            print(os.path.join(directory, filename))
+        else:
+            continue
 
 
 if __name__ == '__main__':
-    fileNames = getFileNames()  # The directory is currently hardcoded
+    """fileNames = getFileNames()  # The directory is currently hardcoded
     for file in fileNames:
         text = summarizeText(getText(file))
-        pprint(f"{file}:\t{text}", end="\n-----------------\n")
+        pprint(f"{file}:\t{text}", end="\n-----------------\n")"""
+    print(getFileNames())
