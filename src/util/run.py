b716d3124779b49976d5c7dc3276a0b1c64c21fc
enpuyou <youe2@allegheny.edu>
2019-12-19 15:18:27 -0500

    Fix import typo

--- src/util/run.py
+++ src/util/run.py
@@ -1,9 +1,9 @@
 """Entry point"""
 import sys
-from analyzer import normalize, read_file, word_freq, computeTfIDF
+from analyzer import normalize, read_file, word_freq, compute_TfIDF
 
 if __name__ == "__main__":
     TFIDF = normalize(read_file(sys.argv[1]))
     FREQ = word_freq(read_file(sys.argv[1]))
     # print(FREQ)
-    computeTfIDF(TFIDF)
+    compute_TfIDF(TFIDF)
