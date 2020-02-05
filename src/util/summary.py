7ebeace472891d610d13663d301e6b7aa93bf0d6
enpuyou <youe2@allegheny.edu>
2020-02-05 00:37:26 -0500

    Remane files

--- src/util/summary.py
+++ src/util/summary.py
@@ -3,24 +3,28 @@ from pprint import pprint
 import os
 
 
-def summarizeText(text: str) -> str:
+def summarize_text(text: str) -> str:
     """ Uses genim's summarization to summarize the given text """
     return summarize(text, word_count=30)
 
 
-def getText(fileName: str) -> str:
+def get_text(fileName: str) -> str:
     """ Returns selected passages from the file after given a file name """
     with open(fileName, "r") as file:
         text = file.read()
 
-    text = text.replace("## What was the greatest technical challenge that your team faced and how did you overcome it?",
-    "## Based on your experiences with simple DNA manipulation in this lab and reflecting on the assigned article, answer the following questions:")
+    text = text.replace(
+        "## What was the greatest technical challenge that your team faced and how did you overcome it?",
+        "## Based on your experiences with simple DNA manipulation in this lab and reflecting on the assigned article, answer the following questions:",
+    )
 
-    a = text.split("## Based on your experiences with simple DNA manipulation in this lab and reflecting on the assigned article, answer the following questions:")
+    a = text.split(
+        "## Based on your experiences with simple DNA manipulation in this lab and reflecting on the assigned article, answer the following questions:"
+    )
     return a[1]
 
 
-def getFileNames() -> [str]:
+def get_file_names() -> [str]:
     """ Uses os library to find all markdown files in given directory """
     fileList = []
     directory = "cs100f2019_lab05_reflections"
@@ -35,13 +39,13 @@ def getFileNames() -> [str]:
 
 
 def summarizer():
-    fileNames = getFileNames()  # The directory is currently hardcoded
+    fileNames = get_file_names()  # The directory is currently hardcoded
     for file in fileNames:
-        text = summarizeText(getText(file))
+        text = summarize_text(get_text(file))
         print(f"{file}:\t{text}", end="\n-----------------\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     summarizer()
 
 # TODO find an easier way to automate collecting the actual text
