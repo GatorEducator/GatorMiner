efb27dbb8b41003cf4c233d02226f5f1c8128814
enpuyou <youe2@allegheny.edu>
2020-02-06 23:16:46 -0500

    Remove unused code

--- src/util/run.py
+++ src/util/run.py
@@ -1,16 +1,10 @@
 """Entry point"""
 import sys
 from analyzer import normalize, read_file, word_freq, compute_TfIDF
-from summary import summarizer
+
 
 if __name__ == "__main__":
     TFIDF = normalize(read_file(sys.argv[1]))
     FREQ = word_freq(read_file(sys.argv[1]))
     # print(FREQ)
     compute_TfIDF(TFIDF)
-    print("\n\n")
-    print("___________________________________________")
-    print("Printing synopsis of student reflections...")
-    print("___________________________________________")
-    print("\n\n")
-    summarizer()
