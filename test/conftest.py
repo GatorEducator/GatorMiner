7abdea193c1e7c5d89ac3e97cd6221445c1fba75
enpuyou <youe2@allegheny.edu>
2020-02-07 00:29:43 -0500

    Update conftest

--- test/conftest.py
+++ test/conftest.py
@@ -4,8 +4,7 @@ import os
 import sys
 
 GO_BACK_A_DIRECTORY = "/../"
-GO_INTO_SRC_DIRECTORY = "src"
 
 # set the system path to contain the previous directory
 PREVIOUS_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, PREVIOUS_DIRECTORY + GO_BACK_A_DIRECTORY + GO_INTO_SRC_DIRECTORY)
+sys.path.insert(0, PREVIOUS_DIRECTORY + GO_BACK_A_DIRECTORY)
