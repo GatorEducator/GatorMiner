c97660ab561c4099c10423aca4c6b7a01fface4c
enpuyou <youe2@allegheny.edu>
2020-02-12 20:45:38 -0500

    Add test cases to test merge data

--- test/test_summarizer.py
+++ test/test_summarizer.py
@@ -55,6 +55,54 @@ system."
     assert expected == output
 
 
+def test_merge_data_with_two_inputs(tmp_path):
+    """Test that summarizer pipeline works"""
+    d = tmp_path / "sub"
+    d.mkdir()
+    p1 = d / "hello.md"
+    p2 = d / "world.md"
+    text = "Some solutions that can be developed to \
+avoid harm or fix the harm are conducting more research and not offering it \
+to a selective group of people. More research needs to be done especially in \
+terms of embryos. In addition, if germline editing is only offered to a \
+select group of people, the wealthy, it will be problematic for the class \
+system."
+    p1.write_text(f"# Reflection by\n\n## header1\n{text}\n## header2\n{text}")
+    p2.write_text(f"# Reflection by\n\n## header1\n{text}\n## header2\n{text}")
+    expected = {
+        "Reflection by": ["", ""],
+        "header1": [text + " ", text + " "],
+        "header2": [text + " ", text + " "],
+    }
+    output = summarizer.merge_data(d)
+    assert expected == output
+
+
+def test_merge_data_with_three_inputs(tmp_path):
+    """Test that summarizer pipeline works"""
+    d = tmp_path / "sub"
+    d.mkdir()
+    p1 = d / "hello.md"
+    p2 = d / "world.md"
+    p3 = d / "python.md"
+    text = "Some solutions that can be developed to \
+avoid harm or fix the harm are conducting more research and not offering it \
+to a selective group of people. More research needs to be done especially in \
+terms of embryos. In addition, if germline editing is only offered to a \
+select group of people, the wealthy, it will be problematic for the class \
+system."
+    p1.write_text(f"# Reflection by\n\n## header1\n{text}\n## header2\n{text}")
+    p2.write_text(f"# Reflection by\n\n## header1\n{text}\n## header2\n{text}")
+    p3.write_text(f"# Reflection by\n\n## header1\n{text}\n## header2\n{text}")
+    expected = {
+        "Reflection by": ["", "", ""],
+        "header1": [text + " ", text + " ", text + " "],
+        "header2": [text + " ", text + " ", text + " "],
+    }
+    output = summarizer.merge_data(d)
+    assert expected == output
+
+
 def test_summarizer_with_two_inputs(tmp_path):
     """Test that summarizer pipeline works"""
     d = tmp_path / "sub"
