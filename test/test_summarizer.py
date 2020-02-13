81413936d5958cfd0190abdac6af68ccea00d99a
enpuyou <youe2@allegheny.edu>
2020-02-12 20:36:42 -0500

    Add test case for more than two inputs

--- test/test_summarizer.py
+++ test/test_summarizer.py
@@ -78,3 +78,30 @@ it to a selective group of people."
         "header2": [summrized_text, summrized_text],
     }
     assert expected == output
+
+
+def test_summarizer_with_three_inputs(tmp_path):
+    """Test that summarizer pipeline works"""
+    d = tmp_path / "sub"
+    d.mkdir()
+    p1 = d / "hello.md"
+    p2 = d / "world.md"
+    p3 = d / "python.md"
+    text = "Some solutions that can be developed to \
+avoid harm or fix the harm are conducting more research and not offering it \
+to a selective group of people. More research needs to be done especially in \
+terms of embryos. In addition, if germline editing is only offered to a \
+select group of people, the wealthy, it will be problematic for the class \
+system."
+    p1.write_text(f"# Reflection by\n\n## header1\n{text}\n## header2\n{text}")
+    p2.write_text(f"# Reflection by\n\n## header1\n{text}\n## header2\n{text}")
+    p3.write_text(f"# Reflection by\n\n## header1\n{text}\n## header2\n{text}")
+    output = summarizer.summarizer(d)
+    summrized_text = "Some solutions that can be developed to avoid harm or \
+fix the harm are conducting more research and not offering \
+it to a selective group of people."
+    expected = {
+        "header1": [summrized_text, summrized_text, summrized_text],
+        "header2": [summrized_text, summrized_text, summrized_text],
+    }
+    assert expected == output
