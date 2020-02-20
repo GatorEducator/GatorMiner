ba12f395405f82926b6de2c8b86076a5e5add3ad
enpuyou <youe2@allegheny.edu>
2020-02-20 00:18:45 -0500

    Clean up the import statement

--- textmining.py
+++ textmining.py
@@ -3,16 +3,15 @@ import sys
 
 # from analyzer import normalize, read_file, word_freq, compute_TfIDF
 # import summarizer as summarize
-import src.util.analyzer as az
-import src.util.summarizer as sz
+from src.util import analyzer as az
+from src.util import summarizer as sz
 from src.util import arguments
-import pprint
 
 if __name__ == "__main__":
     tm_arguments = arguments.parse(sys.argv[1:])
     directory = tm_arguments.directory
     function = tm_arguments.function
     if function == "frequency":
-        # print(summarize("cs100f2019_lab05_reflections"))
         print(az.dir_frequency(directory))
-    # print(sz.summarizer("cs100f2019_lab05_reflections"))
+    elif function == "summarize":
+        print(sz.summarizer(directory))
