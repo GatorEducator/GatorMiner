096672bbe24357257d0e0efbd50cae639703c130
enpuyou <youe2@allegheny.edu>
2020-03-22 00:35:18 -0400

    Add optional argument

--- src/analyzer.py
+++ src/analyzer.py
@@ -35,10 +35,10 @@ def compute_frequency(token_lst: List[str], amount=50) -> List[Tuple[str, int]]:
     return word_freq.most_common(amount)
 
 
-def word_frequency(text: str) -> List[Tuple[str, int]]:
+def word_frequency(text: str, amount=50) -> List[Tuple[str, int]]:
     """A pipeline to normalize, tokenize, and
     find word frequency of raw text"""
-    return compute_frequency(tokenize(normalize(text)))
+    return compute_frequency(tokenize(normalize(text)), amount)
 
 
 def dir_frequency(dirname: str, amount=50) -> List[Tuple[str, int]]:
