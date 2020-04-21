2d9459d6138e60c47cab2f95078bc6b536ec213a
enpuyou <youe2@allegheny.edu>
2020-04-21 00:07:26 -0400

    Add basic code structure for topic modeling

---
+++ src/topic_modeling.py
@@ -0,0 +1,32 @@
+from gensim import corpora
+import analyzer as az
+import markdown as md
+import pickle
+import gensim
+
+text_data = [
+    az.tokenize(
+        az.normalize(
+            md.read_file("../resources/cs100f2019_lab05_reflections/reflection1.md")
+        )
+    )
+]
+print(text_data)
+
+dictionary = corpora.Dictionary(text_data)
+# print(dictionary)
+corpus = [dictionary.doc2bow(text) for text in text_data]
+
+pickle.dump(corpus, open("corpus.pkl", "wb"))
+dictionary.save("dictionary.gensim")
+
+NUM_TOPICS = 5
+ldamodel = gensim.models.ldamodel.LdaModel(
+    corpus, num_topics=NUM_TOPICS, id2word=dictionary, passes=15
+)
+ldamodel.save("model5.gensim")
+
+topics = ldamodel.print_topics(num_words=4)
+for topic in topics:
+    print(topic)
+# dictionary = dictionary.doc2bow(text_data)
