a6f3350b6d2943bafbc3a9cbc425473031428acf
enpuyou <youe2@allegheny.edu>
2020-04-21 00:35:49 -0400

    Add in function

--- src/topic_modeling.py
+++ src/topic_modeling.py
@@ -1,31 +1,31 @@
-import src.analyzer as az
-import src.markdown as md
+import analyzer as az
+import markdown as md
 import pickle
 import gensim
+from typing import List, Tuple
 
-text_data = [
-    az.tokenize(
-        az.normalize(
-            md.read_file("../resources/cs100f2019_lab05_reflections/reflection1.md")
-        )
+input = "../resources/cs100f2019_lab05_reflections/reflection1.md"
+
+
+def topic_model(input, NUM_TOPICS=5, NUM_WORDS=4) -> List[Tuple[int, str]]:
+    text_data = [az.tokenize(az.normalize(md.read_file(input)))]
+    # Create Dictionary by giving id to each word
+    id2word = gensim.corpora.Dictionary(text_data)
+
+    # Term Document Frequency
+    corpus = [id2word.doc2bow(text) for text in text_data]
+
+    # Build LDA model
+    ldamodel = gensim.models.ldamodel.LdaModel(
+        corpus, num_topics=NUM_TOPICS, id2word=id2word, passes=15
     )
-]
-print(text_data)
 
-dictionary = gensim.corpora.Dictionary(text_data)
-# print(dictionary)
-corpus = [dictionary.doc2bow(text) for text in text_data]
+    # pickle.dump(corpus, open("corpus.pkl", "wb"))
+    # id2word.save("dictionary.gensim")
+    # ldamodel.save("model5.gensim")
 
-pickle.dump(corpus, open("corpus.pkl", "wb"))
-dictionary.save("dictionary.gensim")
+    topics = ldamodel.print_topics(num_words=NUM_WORDS)
+    # for topic in topics:
+    #     print(topic)
 
-NUM_TOPICS = 5
-ldamodel = gensim.models.ldamodel.LdaModel(
-    corpus, num_topics=NUM_TOPICS, id2word=dictionary, passes=15
-)
-ldamodel.save("model5.gensim")
-
-topics = ldamodel.print_topics(num_words=4)
-for topic in topics:
-    print(topic)
-# dictionary = dictionary.doc2bow(text_data)
+    return topics
