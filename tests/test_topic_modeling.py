9ce0efac32733b09db94900ddc4bb8f11b6e6951
enpuyou <youe2@allegheny.edu>
2020-04-21 01:42:58 -0400

    Add a test case for topic modeling function

--- tests/test_topic_modeling.py
+++ tests/test_topic_modeling.py
@@ -1,2 +1,14 @@
 """Test module for topic_modeling.py"""
-import src.summarizer as summarizer
+import src.topic_modeling as tm
+
+
+def test_topic_modeling_produce_output():
+    """Test that summarize text works"""
+    input_text = "Some solutions that can be developed to avoid harm or fix \
+the harm are conducting more research and not offering it \
+to a selective group of people. More research needs to be \
+done especially in terms of embryos. In addition, if germline \
+editing is only offered to a select group of people, \
+the wealthy, it will be problematic for the class system."
+    output = tm.topic_model(input_text)
+    assert output is not None
