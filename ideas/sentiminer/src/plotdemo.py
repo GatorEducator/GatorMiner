000bd0b30401650783538b55f8af97c16a661b35
enpuyou <youe2@allegheny.edu>
2020-04-22 00:29:23 -0400

    run black reformat

--- ideas/sentiminer/src/plotdemo.py
+++ ideas/sentiminer/src/plotdemo.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
@@ -19,23 +18,35 @@ index = np.arange(n_groups)
 bar_width = 0.35
 
 opacity = 0.4
-error_config = {'ecolor': '0.3'}
+error_config = {"ecolor": "0.3"}
 
-rects1 = ax.bar(index, means_men, bar_width,
-                alpha=opacity, color='b',
-                yerr=std_men, error_kw=error_config,
-                label='Men')
+rects1 = ax.bar(
+    index,
+    means_men,
+    bar_width,
+    alpha=opacity,
+    color="b",
+    yerr=std_men,
+    error_kw=error_config,
+    label="Men",
+)
 
-rects2 = ax.bar(index + bar_width, means_women, bar_width,
-                alpha=opacity, color='r',
-                yerr=std_women, error_kw=error_config,
-                label='Women')
+rects2 = ax.bar(
+    index + bar_width,
+    means_women,
+    bar_width,
+    alpha=opacity,
+    color="r",
+    yerr=std_women,
+    error_kw=error_config,
+    label="Women",
+)
 
-ax.set_xlabel('Group')
-ax.set_ylabel('Scores')
-ax.set_title('Scores by group and gender')
+ax.set_xlabel("Group")
+ax.set_ylabel("Scores")
+ax.set_title("Scores by group and gender")
 ax.set_xticks(index + bar_width / 2)
-ax.set_xticklabels(('A', 'B', 'C', 'D', 'E'))
+ax.set_xticklabels(("A", "B", "C", "D", "E"))
 ax.legend()
 
 fig.tight_layout()
