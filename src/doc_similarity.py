4b5c5e1561ffee3a1ba2226bc9683924c9a6b968
enpuyou <youe2@allegheny.edu>
2020-04-21 22:51:02 -0400

    update similarity function

--- src/doc_similarity.py
+++ src/doc_similarity.py
@@ -1,33 +1,5 @@
-from scipy.spatial import distance
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-
-
-def cosine_similarity(pair):
-
-    doc_1, doc_2 = pair
-
-    # text to vector
-    vectorizer = CountVectorizer()
-    all_sentences_to_vector = vectorizer.fit_transform([doc_1, doc_2])
-    text_to_vector_v1 = all_sentences_to_vector.toarray()[0].tolist()
-    text_to_vector_v2 = all_sentences_to_vector.toarray()[1].tolist()
-
-    # distance of similarity
-    cosine = distance.cosine(text_to_vector_v1, text_to_vector_v2)
-    print(
-        "Similarity of two sentences are equal to ", round((1 - cosine) * 100, 2), "%"
-    )
-    return cosine
-
-
-# text_lst = ["normalized text1", "normalized text2"]
-# vectorizer = TfidfVectorizer(min_df=0.0, max_df=1.0, ngram_range=(1,1))
-#
-# # calculate the feature matrix
-# feature_matrix = vectorizer.fit_transform(text_lst).astype(float)
-
-
-key_lst = ["author 1", "author 2", "author 3", "author 4", "author 5"]
+import numpy as np
 
 
 def create_pair(key_lst):
@@ -38,6 +10,28 @@ def create_pair(key_lst):
             pairs.append((v, key))
 
     return pairs
+
+
+def tfidf_cosine_similarity(pair):
+
+    doc_1, doc_2 = pair
+
+    # text to vector
+    vectorizer = TfidfVectorizer(min_df=0.0, max_df=1.0, ngram_range=(1, 1))
+    # calculate the feature matrix
+    feature_matrix = vectorizer.fit_transform([doc_1, doc_2]).astype(float)
+    doc_v1 = feature_matrix.toarray()[0].tolist()
+    doc_v2 = feature_matrix.toarray()[1].tolist()
+
+    # compute cosine similarity manually
+    cosine_similarity = np.dot(doc_v1, doc_v2)
+
+    return cosine_similarity
+
+
+text_lst = ["normalized text1", "normalized text2"]
+
+key_lst = ["author 1", "author 2", "author 3", "author 4", "author 5"]
 
 
 print(create_pair(key_lst))
