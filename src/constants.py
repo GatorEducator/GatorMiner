568c718ce3185ee0775b896701eb65dabcbef709
enpuyou <youe2@allegheny.edu>
2020-07-03 19:07:41 -0400

    Add gradle report keys and extensions

---
+++ src/constants.py
@@ -0,0 +1,14 @@
+"""Constants used in textmining"""
+
+# Gradle report
+REPORT_ID = "userID"
+REPORT_REFLECTION = "reflection"
+REPORT_ASSIGNMENT = "assignment"
+REPORT_KEYS = (REPORT_ASSIGNMENT, REPORT_REFLECTION, REPORT_ID)
+
+
+# Extensions
+JSON_EXT = ".json"
+PYTHON_EXT = ".py"
+TXT_EXT = ".txt"
+MD_EXT = ".md"
