a58168e1b9bb2d385f0727212af6c2187c322f0d
enpuyou <youe2@allegheny.edu>
2020-07-03 19:42:42 -0400

    Refactor json parsing function

--- src/json_util.py
+++ src/json_util.py
@@ -12,30 +12,20 @@ def get_json_files(dir_path):
         for js in os.listdir(dir_path)
         if js.endswith(cts.JSON_EXT)
     ]
-    json_lst = []
+    raw_json_lst = []
     for file in json_files:
         with open(file) as file:
-            json_lst.append(json.load(file))
-    return json_lst
+            raw_json_lst.append(json.load(file))
+    return raw_json_lst
 
 
 def clean_report(raw_json_lst):
-    """Filter out unwanted key items in the report"""
+    """Filter out unwanted key items and updated with md parsing"""
     clean_json = []
     for js in raw_json_lst:
-        clean_json.append(
-            {k: v for k, v in js.items() if k in cts.REPORT_KEYS})
+        filtered = {k: v for k, v in js.items() if k in cts.REPORT_KEYS}
+        md_dict = md.md_parser(filtered[cts.REPORT_REFLECTION])
+        filtered.update({"combined": " ".join(md_dict.values())})
+        filtered.update(md_dict)
+        clean_json.append(filtered)
     return clean_json
-
-
-def parse_md(json_lst):
-    for js in json_lst:
-        js.update(md.md_parser(js[cts.REPORT_REFLECTION]))
-    return json_lst
-
-
-if __name__ == "__main__":
-    print(
-        parse_md(clean_report(get_json_files("../resources/sample_report")))[0]
-    )
-    # print(jsonfiles)
