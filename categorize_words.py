c208849d4485d871f3790aa6842488c5fbf5a751
Kiley <>
2021-03-31 14:07:59 -0400

    Added docstrings

--- categorize_words.py
+++ categorize_words.py
@@ -1,15 +1,16 @@
+"""Categorize words according to unique mentions in correspondent assignment categories"""
 categories = []
 
 def input_file():
-    # prompt user for file and its categories
+    """retrieve file and its categories from user input"""
     filename = input("Enter path of assignment file: ")
     category_input = input("Enter categories of assignment separated by comma and space: ")
-
     # normalize input
     global categories
     categories = category_input.split(', ')
 
 def classify_file_categories():
+    """add category to list of categories if necessary"""
     with open("category_frequencies/categories.txt", "r") as file_categories:
         original_list = file_categories.read()
         with open("category_frequencies/categories.txt", "a") as category_list:
