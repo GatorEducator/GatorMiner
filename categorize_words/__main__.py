b816c40b921057d4be27afba079ace053473e4aa
donizk <donizk@allegheny.edu>
2021-04-01 03:06:19 -0400

    added notes (as comments) to myself onto the __main__.py file to keep track of ideas for implementation

--- categorize_words/__main__.py
+++ categorize_words/__main__.py
@@ -21,6 +21,9 @@ def main(file: str = typer.Option(...), category: str = typer.Option(...)):
     # prompt about categories & command-line response
     typer.echo("What category would you like to group your files under?")
     typer.echo("")
+    # Note to self (NTS): Can we use an iteration construct here (in order to collect multiple categories)
+    # NTS: will what is entered by the user here work with what Kiley has?
+    # NTS: this uses command line to define each of these variables before the program is run, is this the most efficient method for our purposes?
     typer.echo(f"{category} is a category you would like to sort files by.")
     typer.echo(
         "Thank you for using our program, we hope that this was helpful for developing categories for assignments."
