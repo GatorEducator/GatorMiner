63015dc6a1ce4d8ce412d08984409f857b14d2c9
enpuyou <youe2@allegheny.edu>
2021-04-05 19:40:58 -0400

    Move docker script to scripts

---
+++ scripts/docker_run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# printf "\n[+] (Mac) Running Docker Container"
+# docker container run --name devi -d -p 8501:8501 bennywestsyde/gatorminer && echo "\n\tYou can now view your Streamlit app in your browser.\n\n\tNetwork URL: http://localhost:8501 \n\tExternal URL: http://141.195.4.17:8501"
+docker run -ti -d -p 8080:8501 gatorminer
