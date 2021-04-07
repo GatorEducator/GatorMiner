d84d59397aec38b568d849fa6603dc6ba47dbba9
colemank <colemank@allegheny.edu>
2021-04-07 13:42:51 -0400

    Comment source code

--- streamlit_web.py
+++ streamlit_web.py
@@ -358,6 +358,7 @@ def question_freq(freq_range):
 def sentiment():
     """main function for sentiment analysis"""
     senti_df = main_df.copy(deep=True)
+    # Initializing the new columns with a numpy array, so the entire series is returned
     senti_df["Positive words"] = senti_pos_iter(senti_df["tokens"].values)
     senti_df["Negative words"] = senti_neg_iter(senti_df["tokens"].values)
 
@@ -385,13 +386,17 @@ def sentiment():
         question_senti(senti_df)
 
 def senti_pos_iter(tokens_column):
+    # Start off with an empty list
     display_series = []
+    # For each item in the already existing tokens column in the data frame,
     for token_element in tokens_column:
-        display = []
+        # Start off with an empty words list to add to over time
         words = []
+        # If the word isn't in the words list already, then it's added to the words list
         for word in token_element:
             if word not in words:
                 words.append(word)
+        # Insertion sort algorithm using the polarity to compare the words
         for i in range(len(words)):
             key = words[i]
             j = i - 1
@@ -399,17 +404,15 @@ def senti_pos_iter(tokens_column):
                 words[j + 1] = words[j]
                 j -= 1
             words[j + 1] = key
-
-        for word in words:
-            display.append(word)
-        display_series.append(", ".join(display[len(display) - 3: len(display)]))
-
+        # Add the words at the end of the list to the display series since they have the most positive sentiment value
+        display_series.append(", ".join(words[len(words) - 3: len(words)]))
+    # Return an entire series based on the display_series list
     return pd.Series(display_series)
 
 def senti_neg_iter(tokens_column):
+    #Same as senti_pos_iter
     display_series = []
     for token_element in tokens_column:
-        display = []
         words = []
         for word in token_element:
             if word not in words:
@@ -421,10 +424,8 @@ def senti_neg_iter(tokens_column):
                 words[j + 1] = words[j]
                 j -= 1
             words[j + 1] = key
-
-        for word in words:
-            display.append(word)
-        display_series.append(", ".join(display[0:3]))
+        # Add the words at the beginning of the list to the display series since they have the most negative sentiment value
+        display_series.append(", ".join(words[0:3]))
 
     return pd.Series(display_series)
 
