ff5bf895c0ebbded20c6f7cbfb575b444245fac3
m <m@ms-MacBook-Air.local>
2021-04-07 13:14:43 -0400

    Turn grammar analyzer to dictionary

--- src/grammar_analyzer.py
+++ src/grammar_analyzer.py
@@ -1,24 +1,22 @@
 import language_check
+from . import markdown as md
+from typing import Dict, List
 
 # Mention the language keyword
-def grammar_analyzer(data: str) -> str:
+def grammar_analyzer(directory: str) -> Dict[str, List[int][str]]:
     '''A tool to check grammar error in reflection'''
     tool = language_check.LanguageTool('en-US')
     i = 0
-
-    # Path of file which needs to be checked will be parse to data in main method
-    with data as file:
-        for line in file:
-            matches = tool.check(line)
-            i = i + len(matches)
-            pass
-
-    # prints total mistakes which are found
-    # from the document
-    print("No. of mistakes found in document is ", i)
-    print()
-
-    # prints mistake one by one
-    for mistake in matches:
-        print(mistake)
-        print()
+    # Parse the file which needs to be checked
+    main_md_dict = md.collect_md(directory, is_clean=False)
+    # initialize summarized dict with keys in sources
+    grammar_checker = {k: [][] for k in main_md_dict.keys()}
+    for key, values in main_md_dict.items():
+        for item in values:
+            for line in item:
+                    matches = tool.check(line)
+                    i = i + len(matches)
+                    grammar_checker[key].append(i)
+                    for mistake in matches
+                        grammar_checker[key][i].append(mistake)
+    return grammar_checker
