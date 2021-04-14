d69ac169ce6492f918a83d4d685055c5ad7f746c
m <m@ms-MacBook-Air.local>
2021-04-14 08:03:12 -0400

    Fixing flake8 error and add comment to src code

--- src/grammar_analyzer.py
+++ src/grammar_analyzer.py
@@ -1,6 +1,5 @@
 # pylint: disable=missing-docstring
 import language_tool_python
-from . import markdown as md
 from typing import Dict, List
 
 # Mention the language keyword
@@ -10,9 +9,9 @@ def grammar_analyzer(text: str) -> Dict[int, List[str]]:
     '''A tool to check grammar error in reflection'''
     tool = language_tool_python.LanguageTool('en-US')
     i = 0
-    # Parse the file which needs to be checked
+    # store the number of errors as keys and errors as items of a dictionary
     grammar_err = {}
-    # initialize summarized dict with keys in sources
+    # initialize key and item into grammar_err dictionary
     for line in text:
         matches = tool.check(line)
         i = i + len(matches)
