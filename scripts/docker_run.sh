75e210084cebb0cf2ee73cf7318badffe1f5ee9f
enpuyou <youe2@allegheny.edu>
2021-04-14 03:03:48 -0400

    Change port to 8501

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -3,4 +3,4 @@ printf "\n[+] (Mac) Running Docker Container"
 docker container run --name devi -d -p 8501:8501 bennywestsyde/gatorminer && echo "\n\tYou can now view your Streamlit app in your browser.\n\n\tNetwork URL: http://localhost:8501 \n\tExternal URL: http://141.195.4.17:8501"
 
 # Run gatorminer container from the local image
-# docker run -it -d -p 8080:8501 gatorminer
+# docker run -it -d -p 8501:8501 gatorminer
