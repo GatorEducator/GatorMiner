5d0443e3948c856e9e070a32d3c31af3081e0b54
m <m@ms-MacBook-Air.local>
2021-04-14 15:50:02 -0400

    Fixing source code

--- src/grammar_analyzer.py
+++ src/grammar_analyzer.py
@@ -5,17 +5,18 @@ from typing import Dict, List
 # Mention the language keyword
 
 
-def grammar_analyzer(text: str) -> Dict[int, List[str]]:
+def grammar_analyzer(text: str) -> List[str]:
     '''A tool to check grammar error in reflection'''
     tool = language_tool_python.LanguageTool('en-US')
     i = 0
     # store the number of errors as keys and errors as items of a dictionary
-    grammar_err = {}
+    grammar_err = []
     # initialize key and item into grammar_err dictionary
     for line in text:
         matches = tool.check(line)
         i = i + len(matches)
-        grammar_err.append(i)
-        for mistake in matches:
-            grammar_err[i].append(mistake)
+        pass
+
+    for mistake in matches:
+        grammar_err.append([i, mistake])
     return grammar_err
