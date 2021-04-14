31c4f7491c2c44e1e19620b85d8766b05c5e948e
Nathan Loria <lorian@allegheny.edu>
2021-04-14 15:34:52 -0400

    Updated install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-y=$(eval "which pip")
-
-if [ -f "$y" ]
+if [ -f "$(eval 'which pip')" ]
 then
 	pip_version="pip"
-else
+elif [ -f "$(eval 'which pip3')" ]
+then
 	pip_version="pip3"
+else
+	$(eval "python3 -m pip install --upgrade pip")
+	pip_version="pip"
 fi
 
 echo $(eval "$pip_version install pipenv -U")
+echo $(eval "cd ..")
 echo $(eval "pipenv install")
 echo $(eval "pipenv run python -m spacy download en_core_web_sm")
 echo $(eval "pipenv run python -m spacy download en_core_web_md")
