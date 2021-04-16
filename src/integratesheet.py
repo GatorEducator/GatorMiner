3875c2a184662cef513a460488af0e858fd1445b
zackery <deversz@allegheny.edu>
2021-04-16 09:10:25 -0400

    issue 32

--- src/integratesheet.py
+++ src/integratesheet.py
@@ -4,23 +4,28 @@ from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import json
 
-SCOPE = ["https://spreadsheets.google.com/feeds"]
-SECRETS_FILE = "Pbpython-9c80d2999710.json"
-SPREADSHEET = "PBPython User Survey (Responses)"
+SCOPE = ['https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive',
+    'https://spreadsheets.google.com/feeds']
+SECRETS_FILE = 'data-rookery-309413-5578c70a0a89.json'
+SPREADSHEET = 'Ethical Benefits and Implications (Responses)'
 
 json_key = json.load(open(SECRETS_FILE))
 # Authenticate using the signed key
-credentials = ServiceAccountCredentials.from_json_keyfile_name("Pbpython-9c80d2999710.json", SCOPE)
+credentials =  ServiceAccountCredentials(json_key['client_email'], json_key['private_key'], SCOPE)
 
+#print(credentials)
 gc = gspread.authorize(credentials)
-workbook = gc.open(SPREADSHEET)
+#workbook = gc.open(SPREADSHEET)
 # Get the first sheet
-sheet = workbook.sheet1
-data = pd.DataFrame(sheet.get_all_records())
+#sheet = workbook.sheet1
+#data = pd.DataFrame(sheet.get_all_records())
 
-column_names = {'Timestamp': 'timestamp',
-                'do you want to help': 'help',
-                }
-data.rename(columns=column_names, inplace=True)
-data.timestamp = pd.to_datetime(data.timestamp)
-print(data.head())
+#column_names = {'Timestamp': 'timestamp',
+                #'What future technology is featured in your synopsis?': 'future-tech',
+                #'What are the potential social implications and/or ethical issues and/or regulatory challenges with this technology?': 'ethical-issues',
+                #'What do you think might be a cautionary tale related to this technology?': 'cautionary'
+                #}
+#data.rename(columns=column_names, inplace=True)
+#data.timestamp = pd.to_datetime(data.timestamp)
+#print(data.head())
