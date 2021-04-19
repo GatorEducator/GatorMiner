689206a4f1abf759250b60fa1204be0a78111c46
leflocha <leflocha@allegheny.edu>
2021-04-19 14:00:30 -0400

    this works a little

--- src/pandas.py
+++ src/pandas.py
@@ -13,3 +13,17 @@ def importpanda(import_data):
         raw_df = pd.concat([raw_df, single_df]).fillna("")
     tidy_df = df_preprocess(raw_df)
     return tidy_df, raw_df
+
+def df_preprocess(df):
+    """build and preprocess (combine, normalize, tokenize) text"""
+    # filter out first two columns -- non-report content
+    cols = df.columns[2:]
+    # combining text into combined column
+    df["combined"] = df[cols].apply(
+        lambda row: "\n".join(row.values.astype(str)), axis=1
+    )
+    # normalize
+    df[cts.NORMAL] = df["combined"].apply(lambda row: az.normalize(row))
+    # tokenize
+    df[cts.TOKEN] = df[cts.NORMAL].apply(lambda row: az.tokenize(row))
+    return df
