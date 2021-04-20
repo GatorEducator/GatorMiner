ac94d166f98a1b5b9ef80c8bbaa5c7b5ad82d8ac
zackery <deversz@allegheny.edu>
2021-04-20 09:59:28 -0400

    attempted fixing line 18 w jj

--- src/integratesheet.py
+++ src/integratesheet.py
@@ -14,18 +14,18 @@ json_key = json.load(open(SECRETS_FILE))
 # Authenticate using the signed key
 credentials =  ServiceAccountCredentials(json_key['client_email'], json_key['private_key'], SCOPE)
 
-#print(credentials)
+#print(type(json_key['private_key']))
 gc = gspread.authorize(credentials)
-#workbook = gc.open(SPREADSHEET)
+workbook = gc.open(SPREADSHEET)
 # Get the first sheet
-#sheet = workbook.sheet1
-#data = pd.DataFrame(sheet.get_all_records())
+sheet = workbook.sheet1
+data = pd.DataFrame(sheet.get_all_records())
 
-#column_names = {'Timestamp': 'timestamp',
-                #'What future technology is featured in your synopsis?': 'future-tech',
-                #'What are the potential social implications and/or ethical issues and/or regulatory challenges with this technology?': 'ethical-issues',
-                #'What do you think might be a cautionary tale related to this technology?': 'cautionary'
-                #}
-#data.rename(columns=column_names, inplace=True)
-#data.timestamp = pd.to_datetime(data.timestamp)
-#print(data.head())
+column_names = {'Timestamp': 'timestamp',
+                'What future technology is featured in your synopsis?': 'future-tech',
+                'What are the potential social implications and/or ethical issues and/or regulatory challenges with this technology?': 'ethical-issues',
+                'What do you think might be a cautionary tale related to this technology?': 'cautionary'
+                }
+data.rename(columns=column_names, inplace=True)
+data.timestamp = pd.to_datetime(data.timestamp)
+print(data.head())
