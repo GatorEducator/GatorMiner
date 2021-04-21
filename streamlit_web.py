cca35ea10a60df8cfcc558e5079d70f7c4c09902
TheShiny1 <shinomiyaa@allegheny.edu>
2021-04-20 22:18:18 -0400

    adding student selection for grammar analyzer display

--- streamlit_web.py
+++ streamlit_web.py
@@ -658,7 +658,11 @@ def interactive():
 
 def grammar_analyzer():
     '''Display grammar checker'''
-    """page for grammar error checker"""
+    """page for grammar error checker"""\
+    students = st.multiselect(
+        label="Select specific students below:",
+        options=input_df[stu_id].unique(),
+    )
 
     ga_df = preprocessed_df[
         preprocessed_df[assign_id].isin(assignments)
