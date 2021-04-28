ee4c16c3c0b953134b4d95341f1458abd0b7adb6
Kiley <>
2021-04-27 23:29:07 -0400

    Restore textmining to original

--- textmining.py
+++ textmining.py
@@ -1,12 +1,5 @@
 """CLI Entry point"""
 import sys
-import pandas as pd
-import numpy as np
-import nltk
-import os
-import nltk.corpus
-from nltk.probability import FreqDist
-
 
 from src import analyzer as az
 from src import summarizer as sz
@@ -20,16 +13,3 @@ if __name__ == "__main__":
         print(az.dir_frequency(directory))
     elif function == "summary":
         print(sz.summarizer(directory))
-
-text = "directory"
-# importing word_tokenize from nltk
-from nltk.tokenize import word_tokenize
-# Passing the string text into word tokenize for breaking the sentences
-token = word_tokenize(text)
-token
-
-fdist = FreqDist(token)
-fdist
-
-fdist1 = fdist.most_common(10)
-fdist1
