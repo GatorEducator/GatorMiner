67042a2719458430b45056598428147b6615bf54
enpuyou <youe2@allegheny.edu>
2021-05-27 18:08:07 -0400

    Add js and css to constants

--- src/constants.py
+++ src/constants.py
@@ -27,3 +27,8 @@ NEGATIVE = "Negative words"
 
 # Path
 IMG_DIR = f"resources{os.path.sep}images"
+
+# Style
+HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid \
+#e6e9ef; border-radius: 0.25rem; padding: 1rem; margin-bottom: 2.5rem">\
+{}</div>"""
